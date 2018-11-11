069f19881bb182b426e3d47159327c49cf99da6e
ulstanna <ulst.anna@gmail.com>
2018-11-11 05:00:52 +0200

    noo

--- projekt.py
+++ projekt.py
@@ -1,59 +1,72 @@
 import webbrowser
-from tkinter import *
 from tkinter.ttk import *
+from tkinter import *
 from tkinter import ttk
-import tkinter.messagebox
+from tkinter import messagebox
 
 window = Tk()
 window.title("Profile maker")
 window.geometry('400x200')
 window.iconbitmap('ikoon.ico')
 
+
 #Failis leheküljed.txt esimesel positsioonil on profiili nimi ja teine kuni lõpmatus positsioonil
 # erinevad leheküljed
-#
-#
+
 
 #Funktsioon runni() võtab failist leheküljed.txt ja pärast lehekülgede avamist programm sulgub
 def runni():
-	with open('leheküljed.txt') as f:
-		for lehekulg in f:
-			lehekulg = lehekulg.split(' ')
-			webbrowser.open_new_tab(lehekulg[1])
-	window.destroy()
+    i = 1
+    with open('leheküljed.txt') as f:
+        for rida in f:
+            if rida.split(",")[0] == vali():
+                while i < len(rida.split(",")):
+                    webbrowser.open_new_tab(rida.split(",")[i])
+                    i += 1
+    window.destroy()
 
 #Funktsioon quit() vastab nupule "Sulge"
 def quit():
 	window.destroy()
 
 #Funktsioon uus_profiil() loob
-def uus_profiil():
-
-
-kaivita = Button(text="käivita", command = runni)
-kaivita.place(x=300, y=80)
+##def uus_profiil():
 
 
 def profiililist():
-	profiilid = []
-	with open('leheküljed.txt') as f:
-		for profiil in f:
-			profiil = profiil.split(' ')
-			profiilid.append(profiil[0])
-	return profiilid
+    profiilid = []
+    with open('leheküljed.txt', encoding ='UTF-8') as f:
+        for tegevus in f:
+            tegevus =tegevus.split(',')
+            profiilid.append(tegevus[0].strip())
+    return profiilid
 
-sulge = Button(text="sulge", command = exit)
+def vali():
+    return profiil.get()
+    
+    
+
+kaivita = Button(text="käivita", command = runni)
+kaivita.place(x=300, y=80)
+    
+
+sulge = Button(text="sulge", command = quit)
 sulge.place(x=300, y=140)
 
-loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
-loo_uus.place(x=300, y=110)
+profiil = Combobox(window, values=(profiililist()))
+profiil.current(0)
+profiil.place(x=10, y=40)
+
+
+#selle nupuga saan  kasutaja poolt valitud tegevuse
+value = Button(text="Vali", command = vali)
+value.place(x=10, y=70)
+
+##loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
+##loo_uus.place(x=300, y=110)
 
 pealkiri = Label(window, text = "Vali profiil:")
 pealkiri.place(x=10, y=10)
-
-profiil = Combobox(values=(profiililist()))
-profiil.current(0)
-profiil.place(x=10, y=40)
 
 
 window.mainloop()

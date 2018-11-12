94ee01277f0267078a9915e4ec6bff08eaeda223
Marten Tamm <marten.tamm@gmail.com>
2018-11-12 11:07:14 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ulstjatamm.py

--- projekt.py
+++ projekt.py
@@ -15,32 +15,30 @@ window.iconbitmap('ikoon.ico')
 #
 
 #Funktsioon runni() võtab failist leheküljed.txt ja pärast lehekülgede avamist programm sulgub
-def runni():
+def runni(profiil):
+	print()
 	with open('leheküljed.txt') as f:
 		for lehekulg in f:
-			lehekulg = lehekulg.split(' ')
-			webbrowser.open_new_tab(lehekulg[1])
-	window.destroy()
-
-#Funktsioon quit() vastab nupule "Sulge"
-def quit():
+			lehekulg = lehekulg.split(',')
+			if lehekulg[0] == profiil:
+				webbrowser.open_new_tab(lehekulg[1])
 	window.destroy()
 
 #Funktsioon uus_profiil() loob
 def uus_profiil():
-
-
-kaivita = Button(text="käivita", command = runni)
-kaivita.place(x=300, y=80)
-
+	pass
 
 def profiililist():
 	profiilid = []
 	with open('leheküljed.txt') as f:
 		for profiil in f:
-			profiil = profiil.split(' ')
+			profiil = profiil.split(',')
 			profiilid.append(profiil[0])
 	return profiilid
+
+nimi = StringVar()
+kaivita = Button(text="käivita", command = runni)
+kaivita.place(x=300, y=80)
 
 sulge = Button(text="sulge", command = exit)
 sulge.place(x=300, y=140)
@@ -48,12 +46,15 @@ sulge.place(x=300, y=140)
 loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
 loo_uus.place(x=300, y=110)
 
-pealkiri = Label(window, text = "Vali profiil:")
+pealkiri = Label(text = "Vali profiil:")
 pealkiri.place(x=10, y=10)
 
-profiil = Combobox(values=(profiililist()))
+profiil = Combobox(textvariable = nimi, values=profiililist())
+
+profiil.bind("<<ComboboxSelected>>",runni)
 profiil.current(0)
 profiil.place(x=10, y=40)
+
 
 
 window.mainloop()

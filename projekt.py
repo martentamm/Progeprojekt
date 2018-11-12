1d17a4d8b9f50b56eb0110e6b6f44ab362ee21ea
Marten Tamm <marten.tamm@gmail.com>
2018-11-12 18:22:23 +0200

    Lisasin uue funktsiooni def uus_aken, millega saab profiile lisada POOLIK

--- projekt.py
+++ projekt.py
@@ -17,13 +17,11 @@ def runni():
 				while i < len(rida.split(",")):
 					webbrowser.open_new_tab(rida.split(",")[i])
 					i += 1
+
 	window.destroy()
 
 def quit():
 	window.destroy()
-
-def uus_profiil():
-	pass
 
 def profiililist():
 	profiilid = []
@@ -33,21 +31,80 @@ def profiililist():
 			profiilid.append(tegevus[0].strip())
 	return profiilid
 
+def lisa_uus():
+	uus_profiil = []
+	a = []
+	def lisa():
+		a.append(lehekülje_kast.get())
+		print(a)
+		return a
+
+	def lõpeta():
+		f = open('leheküljed.txt')
+		f.write(uus_profiil)
+		f.close()
+		uus_aken.destroy()
+
+	def kustuta():
+		pass
+
+	uus_aken = Tk()
+	uus_aken.title('Loo uus profiil')
+	uus_aken.iconbitmap('ikoon.ico')
+	uus_aken.geometry('400x400')
+
+	profiili_nimi_label = Label(uus_aken, text="Uue profiili nimi:")
+	profiili_nimi_label.place(x=10, y=20)
+	profiili_nime_kast = Entry(uus_aken)
+	profiili_nime_kast.place(x=110, y=20)
+
+	lehekülje_label = Label(uus_aken, text="Lehekülje aadress:")
+	lehekülje_label.place(x=10, y=40)
+	lehekülje_kast = Entry(uus_aken)
+	lehekülje_kast.place(x=110, y= 40)
+	lisatud_leheküljed = lehekülje_kast.get()
+	box = Listbox(uus_aken)
+	for i in lisa():
+		box.insert(END, i)
+
+	box.config(relief=SUNKEN, border=2)
+	box.place(x=200, y=200)
+
+	lisa = Button(uus_aken, text="Lisa", command=lisa)
+	lisa.place(x=300, y=10)
+	lisa.config(width = 10)
+
+	kustuta = Button(uus_aken, text="kustuta", command=kustuta)
+	kustuta.place(x=300, y=40)
+	kustuta.config(width=10)
+
+	lõpeta = Button(uus_aken, text="Lõpeta", command=lõpeta)
+	lõpeta.place(x=300, y=70)
+	lõpeta.config(width=10)
+
+
+	uus_aken.mainloop()
+
+
 pealkiri = Label(window, text="Vali profiil:")
 pealkiri.place(x=10, y=10)
 
 kaivita = Button(text="käivita", command=runni)
 kaivita.place(x=300, y=80)
+kaivita.config(width=11)
 
 sulge = Button(text="sulge", command=quit)
-sulge.place(x=300, y=140)
+sulge.place(x=300, y=110)
+sulge.config(width=11)
 
 profiil = Combobox(window, values=(profiililist()))
 profiil.current(0)
 profiil.place(x=10, y=40)
 
-##loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
-##loo_uus.place(x=300, y=110)
+loo_uus = Button(text="Loo uus profiil", command = lisa_uus)
+loo_uus.place(x=300, y=140)
+loo_uus.config(width=11)
+
 
 
 window.mainloop()

b65a397dd8079f93111e4128b172988d32c1e4a0
Marten Tamm <marten.tamm@gmail.com>
2018-11-12 14:26:47 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ulstjatamm.py

--- projekt.py
+++ projekt.py
@@ -1,66 +1,71 @@
 import webbrowser
-from tkinter import *
 from tkinter.ttk import *
+from tkinter import *
 from tkinter import ttk
-import tkinter.messagebox
+from tkinter import messagebox
 
 window = Tk()
 window.title("Profile maker")
 window.geometry('400x200')
 window.iconbitmap('ikoon.ico')
 
-#Failis leheküljed.txt esimesel positsioonil on profiili nimi ja teine kuni lõpmatus positsioonil
+
+# Failis leheküljed.txt esimesel positsioonil on profiili nimi ja teine kuni lõpmatus positsioonil
 # erinevad leheküljed
-#
-#Funktsioon runni() võtab failist leheküljed.txt ja pärast lehekülgede avamist programm sulgub
-def runni(event):
-	global profiil
-	valitud_profiil = profiil.get()
+
+
+# Funktsioon runni() võtab failist leheküljed.txt ja pärast lehekülgede avamist programm sulgub
+def runni():
+	i = 1
 	with open('leheküljed.txt') as f:
 		for rida in f:
-			rida = rida.split('||')
-			profiilinimi = rida[0]
-			leheküljed = rida[1]
-			if profiilinimi == valitud_profiil:
-				valitud_lehed = []
-				leheküljed = leheküljed.split(',')
-				print(leheküljed)
-				for i in leheküljed:
-
-				webbrowser.open(leheküljed)
-				break
+			if rida.split(",")[0] == vali():
+				while i < len(rida.split(",")):
+					webbrowser.open_new_tab(rida.split(",")[i])
+					i += 1
 	window.destroy()
 
-#Funktsioon uus_profiil() loob
-def uus_profiil():
-	pass
+
+# Funktsioon quit() vastab nupule "Sulge"
+def quit():
+	window.destroy()
+
+
+# Funktsioon uus_profiil() loob
+##def uus_profiil():
+
 
 def profiililist():
 	profiilid = []
-	with open('leheküljed.txt') as f:
-		for profiil in f:
-			profiil = profiil.split('||')
-			profiilid.append(profiil[0])
+	with open('leheküljed.txt', encoding='UTF-8') as f:
+		for tegevus in f:
+			tegevus = tegevus.split(',')
+			profiilid.append(tegevus[0].strip())
 	return profiilid
 
-nimi = StringVar()
-kaivita = Button(text="käivita", command = runni)
+
+def vali():
+	return profiil.get()
+
+
+kaivita = Button(text="käivita", command=runni)
 kaivita.place(x=300, y=80)
 
-sulge = Button(text="sulge", command = exit)
+sulge = Button(text="sulge", command=quit)
 sulge.place(x=300, y=140)
 
-loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
-loo_uus.place(x=300, y=110)
-
-pealkiri = Label(text = "Vali profiil:")
-pealkiri.place(x=10, y=10)
-
-profiil = Combobox(textvariable = nimi, values=profiililist())
-profiil.bind("<<ComboboxSelected>>",runni)
-# profiil.current(0)
+profiil = Combobox(window, values=(profiililist()))
+profiil.current(0)
 profiil.place(x=10, y=40)
 
+# selle nupuga saan  kasutaja poolt valitud tegevuse
+value = Button(text="Vali", command=vali)
+value.place(x=10, y=70)
 
+##loo_uus = Button(text="Loo uus profiil", command = uus_profiil())
+##loo_uus.place(x=300, y=110)
+
+pealkiri = Label(window, text="Vali profiil:")
+pealkiri.place(x=10, y=10)
 
 window.mainloop()

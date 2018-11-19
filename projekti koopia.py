d05d76979b8229771081443c5f59af07be21ed87
Marten Tamm <marten.tamm@gmail.com>
2018-11-19 17:51:27 +0200

    Tegin veidi tööd uus_aken funktsiooni kallal

--- projekti koopia.py
+++ projekti koopia.py
@@ -1,8 +1,6 @@
 import webbrowser
 from tkinter import *
 from tkinter.ttk import *
-from tkinter import ttk
-import tkinter.messagebox
 
 global window
 window = Tk()
@@ -11,83 +9,87 @@ window.geometry('450x200')
 window.iconbitmap('ikoon.ico')
 
 def lisa_uus():
-	uus_profiil = []
-	a = []
-
-	def lisa():
-		a.append(lehekülje_kast.get())
-		print(a)
-		return a
-
-
-	def lõpeta():
-		f = open('leheküljed.txt')
-		f.write(uus_profiil)
-		f.close()
-		uus_aken.destroy()
-		pass
-
-	def kustuta():
-		pass
 
 	uus_aken = Tk()
 	uus_aken.title('Loo uus profiil')
 	uus_aken.iconbitmap('ikoon.ico')
 	uus_aken.geometry('400x400')
+
+	def lisa():
+		lehekulg = lehekülje_kast.get()
+		box.insert(END, lehekulg)
+
+	def lõpeta():
+		lehekuljed = []
+		profiilinimi = profiili_nime_kast.get()
+		with open('leheküljed.txt', 'a') as lehekuljedtxt:
+			# lehekuljedtxt.write(profiilinimi)
+			start = box.curselection()
+			end = box.curselection()
+
+			# for rida in data:
+			# 	lehekuljedtxt.write(rida + ',')
+			# lehekuljedtxt.write('\n')
+			print(start)
+			uus_aken.destroy()
+
+
+	def kustuta():
+		lehekulg = lehekülje_kast.get()
+		box.delete(END, " ")
+
 	profiili_nimi_label = Label(uus_aken, text="Uue profiili nimi:")
 	profiili_nimi_label.place(x=10, y=20)
 	profiili_nime_kast = Entry(uus_aken)
 	profiili_nime_kast.place(x=110, y=20)
+
 	lehekülje_label = Label(uus_aken, text="Lehekülje aadress:")
 	lehekülje_label.place(x=10, y=40)
+
 	lehekülje_kast = Entry(uus_aken)
 	lehekülje_kast.place(x=110, y= 40)
-	lisatud_leheküljed = lehekülje_kast.get()
-	box = Listbox(uus_aken)
 
-	for i in lisa():
-		box.insert(END, i)
+	box = Listbox(uus_aken)
 
 	box.config(relief=SUNKEN, border=2)
 	box.place(x=200, y=200)
-	lisa = Button(uus_aken, text="Lisa", command=lisa)
+
+	lisa = Button(uus_aken, text="Lisa", command= lisa)
 	lisa.place(x=300, y=10)
 	lisa.config(width = 10)
-	kustuta = Button(uus_aken, text="kustuta", command=kustuta)
+
+	kustuta = Button(uus_aken, text="Kustuta", command= kustuta)
 	kustuta.place(x=300, y=40)
 	kustuta.config(width=10)
-	lõpeta = Button(uus_aken, text="Lõpeta", command=lõpeta)
+
+	lõpeta = Button(uus_aken, text="Lõpeta", command= lõpeta)
 	lõpeta.place(x=300, y=70)
 	lõpeta.config(width=10)
+
+	for rida in uus_profiil:
+		box.insert(rida)
+
 	uus_aken.mainloop()
 
 
 def runni():
-    i = 1
-    with open('leheküljed.txt') as f:
-        for rida in f:
-            if rida.split(",")[0] == profiil.get():
-                while i < len(rida.split(",")):
-                    webbrowser.open_new_tab(rida.split(",")[i])
-                    i += 1
-                if i == len(rida.split(",")):
-                    window.close()
-                
-			    
-					
-#Funktsioon uus_profiil() loob
-def uus_profiil():
-	pass
+	i = 1
+	with open('leheküljed.txt') as f:
+		for rida in f:
+			if rida.split(",")[0] == profiil.get():
+				while i < len(rida.split(",")):
+					webbrowser.open_new_tab(rida.split(",")[i])
+					i += 1
+				if i == len(rida.split(",")):
+					window.close()
 
-
-#Funktsioon profiilist() võtab failist "leheküljed.txt" kõik profiilid ja paneb need järjendisse
 def profiililist():
-    profiilid = []
-    with open('leheküljed.txt', encoding='UTF-8') as f:
-        for rida in f:
-            rida = rida.split(',')
-            profiilid.append(rida[0].strip())
-    return profiilid
+	profiilid = []
+	with open('leheküljed.txt', encoding='UTF-8') as f:
+		for rida in f:
+			rida = rida.split(',')
+			profiilid.append(rida[0].strip())
+	return profiilid
 
 
 nimi = StringVar()
@@ -97,7 +99,7 @@ kaivita.place(x=300, y=80)
 sulge = Button(text="sulge", command = exit)
 sulge.place(x=300, y=140)
 
-loo_uus = Button(text="Loo uus profiil", command =lisa_uus())
+loo_uus = Button(text="Loo uus profiil", command =lisa_uus)
 loo_uus.place(x=300, y=110)
 
 pealkiri = Label(text = "Vali profiil:")
@@ -105,11 +107,7 @@ pealkiri.place(x=10, y=10)
 
 profiil = Combobox(textvariable = nimi, values=profiililist())
 profiil.SelectedIndex = -1
-#profiil.bind("<<ComboboxSelected>>",runni)
-#comboBox.Items.Insert(0, "Palun vali profiil")
-#comboBox1.Text = "Please, select any value"
 profiil.place(x=10, y=40)
-
 
 
 window.mainloop()
